4a015b81d8bd81c051acc1f97dbd4f515e1e8f2c
EmelyanovKonstantin <emelyanovkonstantin@yandex.ru>
2020-03-23 10:26:30 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,43 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  console.log(pathname);
 
   const filepath = path.join(__dirname, 'files', pathname);
-
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+  const countSlash = pathname.split('/').length-1;
+  console.log(filepath);
+  if(countSlash>0){
+    res.statusCode = 400;
+    res.end("Bad Request");
+  }  
+  try {
+    if (fs.existsSync(filepath)) {  
+      switch (req.method) {
+        case 'GET':{
+          const fileStream = fs.createReadStream(filepath);
+          fileStream.pipe(res);
+        }
+    
+          break;
+    
+        default:
+          res.statusCode = 501;
+          res.end('Not implemented');
+      }
+    }else{
+      res.statusCode = 404;
+      res.end('File Not Exist')
+    }
+  }catch(err) {
+    res.statusCode = 500;
+    res.end("Error")
+  }  
 });
 
 module.exports = server;
