003a631b57be8223e1e39611fe8019cf72ba2f94
EmelyanovKonstantin <emelyanovkonstantin@yandex.ru>
2020-03-23 15:50:30 +0300

    solution for 4-module 2-task

---
+++ 4-module/2-task/index.js
@@ -0,0 +1,5 @@
+const server = require('./server');
+
+server.listen(3000, () => {
+  console.log('Serverlistening on http://localhost:3000');
+});
---
+++ 4-module/2-task/writeFile.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const limitSizeStream = require('./LimitSizeStream');
+
+
+module.exports = function writeFile(filepath, req, res){
+  try{
+    if(fs.existsSync(filepath)){
+      res.statusCode = 409;
+      res.end('File exist');
+    }else{
+      const fileStream = fs.createWriteStream(filepath);
+      fileStream.
+        on('finish', function() {          
+          res.statusCode = 201;
+          res.end('done');
+        });
+      res.on('close',()=>{
+        if(res.finished) return;
+        fileStream.destroy();
+        if (fs.existsSync(filepath)){
+          fs.unlink(filepath, (err) => {
+            if (err) throw err;        
+          });
+        }
+      });
+      const sizeStream = new limitSizeStream({limit : 1024*1024});
+      sizeStream.
+       on('error',(err)=>{        
+        if (err.name == "LimitExceededError"){
+          res.statusCode = 413;
+          res.end("Error. Large File.");          
+          if(fs.existsSync(filepath)){
+            fs.unlink(filepath, (err) => {
+              if (err) throw err;        
+            });
+          }
+        }
+       })
+      req.pipe(sizeStream);
+      sizeStream.pipe(fileStream);  
+    }
+  }catch(err){
+    res.statusCode = 500;
+    res.end('Error');
+    console.error(err);
+  }  
+}
