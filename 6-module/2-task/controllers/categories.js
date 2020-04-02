4884e35d5f3dfeb1e45dc5bbb8120fcb83403999
EmelyanovKonstantin <emelyanovkonstantin@yandex.ru>
2020-04-02 20:10:56 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,18 @@
+const Categories = require("../models/Category");
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  let find = await Categories.find({}, '_id subcategories title');
+  let cat = find.map(function(obj) {
+    var sub =  obj.subcategories.map(function(subObj) {
+      return {
+        id : subObj.id,        
+        title : subObj.title
+      }
+    })
+    return {
+      id : obj.id,
+      subcategories : sub,
+      title : obj.title
+    }
+  });   
+  ctx.body = {categories: cat};
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,62 @@
+const Product = require("../models/Product");
+var ObjectId = require('mongoose').Types.ObjectId; 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  if(ctx.query.subcategory){
+    const product = await Product.find({subcategory: ctx.query.subcategory});  
+    ctx.body = product; 
+  }else{
+    const product = await Product.find({});  
+    ctx.body = product;
+  }
+  next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  let body = ctx.body.map(function(obj) {    
+    return {
+      id : obj.id,
+      title : obj.title,
+      images: obj.images,
+      category: obj.category,
+      subcategory : obj.subcategory,
+      price: obj.price,
+      description: obj.description,            
+    }
+  }); 
+  ctx.body = {products: body};
+  next();
+  //ctx.body = {};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+ if(!ObjectId.isValid(ctx.params.id)) ctx.throw(400);
+ const product = await Product.findById(ctx.params.id, (err, result) => {
+  // user is a single document which may be null for no results
+  if (err) {
+    throw err;
+    return;
+  }
+  if (result) {
+    // there is user
+  } else {
+    // there is no user
+  }
+});
+ if(product){
+  let body = {
+    id : product.id,
+    title : product.title,
+    images: product.images,
+    category: product.category,
+    subcategory : product.subcategory,
+    price: product.price,
+    description: product.description,
+   }  
+   ctx.body = {product: body};  
+   next();
+ }else{
+   ctx.throw(404);
+ }
+ 
 };
 
