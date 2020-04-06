b488d868c3c9d7223745e50ee1d0ab8f7bfb08b3
EmelyanovKonstantin <emelyanovkonstantin@yandex.ru>
2020-04-06 15:44:12 +0300

    add second  solution for 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,7 +1,6 @@
 const User = require('../../models/User');
 module.exports = async function authenticate(strategy, email, displayName, done) {
   if(!email) return done(null, false, 'Не указан email');
-  //throw new Error('Не указан email');
   let user = await User.findOne({email: email}, async function(err,user){
     if(!user){      
       await User.create({email: email, displayName: displayName },function (err, user){
